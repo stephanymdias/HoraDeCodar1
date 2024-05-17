bf1456b09694c6afb83bf02f518ad7a67115ec7e
Stephany Marciano Dias <130869859+stephanymdias@users.noreply.github.com>
2024-05-17 09:46:51 -0300

    Add files via upload

---
+++ ex10.js
@@ -0,0 +1,9 @@
+function executarExercicio10(){
+    alert("Área do circulo: ")
+
+    let diametro = parseFloat(prompt("Informe o diâmetro do circulo:"))
+    pi = 3.14
+
+    alert("A área do circulo é: " +(pi * (diametro * diametro)))
+
+}
