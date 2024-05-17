ed06469cba5678cfb3b5398e69f401a1f60c30de
Stephany Marciano Dias <130869859+stephanymdias@users.noreply.github.com>
2024-05-17 09:47:05 -0300

    Delete ex10.js

--- ex10.js
+++
@@ -1,9 +0,0 @@
-function executarExercicio10(){
-    alert("Área do circulo: ")
-
-    let diametro = parseFloat(prompt("Informe o diâmetro do circulo:"))
-    pi = 3.14
-
-    alert("A área do circulo é: " +(pi * (diametro * diametro)))
-
-}
