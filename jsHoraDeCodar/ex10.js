3a30f2c30f415a113bff3afbd8d4304559a0a786
Stephany Marciano Dias <130869859+stephanymdias@users.noreply.github.com>
2024-05-17 09:47:20 -0300

    Add files via upload

--- jsHoraDeCodar/ex10.js
+++ jsHoraDeCodar/ex10.js
@@ -2,6 +2,8 @@ function executarExercicio10(){
     alert("Área do circulo: ")
 
     let diametro = parseFloat(prompt("Informe o diâmetro do circulo:"))
+    pi = 3.14
 
-    alert("A área do circulo é: " )
+    alert("A área do circulo é: " +(pi * (diametro * diametro)))
+
 }
